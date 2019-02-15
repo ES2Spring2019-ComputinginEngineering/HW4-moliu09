2f77bb09707bdb7e7037479badd5630e989cc616
Mo <mo.liu@tufts.edu>
2019-02-15 10:12:32 -0500

    Update cipher_student_version.py

--- cipher_student_version.py
+++ cipher_student_version.py
@@ -36,15 +36,13 @@
 import math
 import time
 message = input("What is your message?\n")
-shift = input("How much do you want to shift the message?\n")
+shift = int(input("How much do you want to shift the message?\n"))
 
 #encrypting function
 def encrypt(message, shift):
     string = ""
-    if str.isdigit(shift) == True:
-        shift = int(shift)
-        for i in message:
-            t = ord(i)
+    for i in message:
+        t = ord(i)
         if t >= 65 and t <= 90:
             t = t + 32
         if t >= 97 and t <= 122:
@@ -52,18 +50,19 @@ def encrypt(message, shift):
             if t > 122:
                 t = 96 + (t - 122)
         string = string + chr(t)
-    else:
-        print("Error! Please type in an integer.")
     return string
-print(encrypt(message, shift))
+print("your message: " + encrypt(message, shift))
+
 
 #automated decrypting function
 time.sleep(1)
 prompt = input("Would you like to decrypt a message?\n")
-if prompt == "yes":
-    m = input("What would you like to decrypt?\n")
-    s = 1
+m = input("What would you like to decrypt?\n")
+s = 1
+def decrypt(m, s):
     while s < 26:
         number = str(s)
         print("amount shifted = " + number + ": " + encrypt(m, s))
         s = s + 1
+if prompt == "yes":
+    print(decrypt(m, s))
